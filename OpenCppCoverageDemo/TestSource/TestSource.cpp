174464186ddb58953a8881dbf9101f901ea3aef6
root <root@BG-O-APP-E-2437.AD.KEYSIGHT.COM>
2018-04-26 19:33:09 -0600

    added more methods, but no unit tests

--- OpenCppCoverageDemo/TestSource/TestSource.cpp
+++ OpenCppCoverageDemo/TestSource/TestSource.cpp
@@ -29,4 +29,84 @@ void SayHello2(string message) {
                 cout << message << endl;
         }
 }
+void SayHello3(string message) {
+        if (message.empty()) {
+                cout << "The input is empty" << endl;
+        }
+        else {
+                cout << message << endl;
+        }
+}
+void SayHello4(string message) {
+        if (message.empty()) {
+                cout << "The input is empty" << endl;
+        }
+        else {
+                cout << message << endl;
+        }
+}
+void SayHello5(string message) {
+        if (message.empty()) {
+                cout << "The input is empty" << endl;
+        }
+        else {
+                cout << message << endl;
+        }
+}
+void SayHello6(string message) {
+        if (message.empty()) {
+                cout << "The input is empty" << endl;
+        }
+        else {
+                cout << message << endl;
+        }
+}
+void SayHello7(string message) {
+        if (message.empty()) {
+                cout << "The input is empty" << endl;
+        }
+        else {
+                cout << message << endl;
+        }
+}
+void SayHello8(string message) {
+        if (message.empty()) {
+                cout << "The input is empty" << endl;
+        }
+        else {
+                cout << message << endl;
+        }
+}
+void SayHello9(string message) {
+        if (message.empty()) {
+                cout << "The input is empty" << endl;
+        }
+        else {
+                cout << message << endl;
+        }
+}
+void SayHello10(string message) {
+        if (message.empty()) {
+                cout << "The input is empty" << endl;
+        }
+        else {
+                cout << message << endl;
+        }
+}
+void SayHello11(string message) {
+        if (message.empty()) {
+                cout << "The input is empty" << endl;
+        }
+        else {
+                cout << message << endl;
+        }
+}
+void SayHello12(string message) {
+        if (message.empty()) {
+                cout << "The input is empty" << endl;
+        }
+        else {
+                cout << message << endl;
+        }
+}
 
