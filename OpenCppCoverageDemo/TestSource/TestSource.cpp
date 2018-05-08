c899ef89287b14b4c4ce5d3f5867494fab829678
root <root@BG-O-APP-E-2437.AD.KEYSIGHT.COM>
2018-05-08 01:14:14 -0600

    changes

--- OpenCppCoverageDemo/TestSource/TestSource.cpp
+++ OpenCppCoverageDemo/TestSource/TestSource.cpp
@@ -19,7 +19,7 @@ void SayHello1(string message) {
 //
         int *p=new int(25);
         delete p;
-        delete p;
+        //delete p;
         if (message.empty()) {
   		std::string str{"123.123.123.123"};
                 cout << "The input is empty" << endl;
